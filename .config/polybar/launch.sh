37aa20bea193f6eb11659d1c446ec5272a776a00
file098 <filippodigennaro98@outlook.it>
2021-09-22 16:24:22 +0200

    big fixes, i fucked up

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+killall -q polybar
+
+sleep 1; polybar --reload bar1 &
+
+echo "launched polybar"
---
+++ .config/polybar/scripts/launchnm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+alacritty -e nmtui &
+sleep 0.5
+bspc node -t floating
