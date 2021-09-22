e5b94e7b991683f830899f8d1f1d7f91254b6155
file098 <filippodigennaro98@outlook.it>
2021-09-22 14:14:02 +0200

    added install script

---
+++ .dotfiles/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+source ~/.zshrc
+echo ".dotfiles.git" >> .gitignore
+git clone --bare https://www.github.com/username/repo.git $HOME/.dotfiles.git
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
