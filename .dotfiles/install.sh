a0c286dcd5894b59f5c2ed630d91bbf3965d206e
file098 <filippodigennaro98@outlook.it>
2021-09-22 15:46:51 +0200

    Update install.sh

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -3,6 +3,6 @@
 echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
 source ~/.zshrc
 echo ".dotfiles.git" >> .gitignore
-git clone --bare https://www.github.com/username/repo.git $HOME/.dotfiles.git
+git clone --bare https://www.github.com/file098/dotfiles.git $HOME/.dotfiles.git
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
