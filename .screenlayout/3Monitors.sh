aed05fd0a8e061876cff4dc70695f4a5a4c0de86
file098 <filippodigennaro98@outlook.it>
2022-02-07 08:56:46 +0100

    deleted some useless stuff

--- .screenlayout/3Monitors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 2944x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output HDMI-1-1 --mode 1280x1024 --pos 0x0 --rotate left --output DP-1-3 --mode 1920x1080 --pos 1024x0 --rotate normal
