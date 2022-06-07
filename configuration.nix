b27965e63e502bc9a5f8854cbd592b26e3bbd793
file098 <filippodigennaro98@outlook.it>
2022-06-07 18:41:51 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,84 +4,78 @@
 
 { config, pkgs, ... }:
 
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./nvidia.nix
+      (import "${home-manager}/nixos")
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  #boot.loader.systemd-boot.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
-#  boot.loader = {
-#      #efi = {
-#      #  canTouchEfiVariables = true;
-#      #  efiSysMountPoint = "/boot";
-#      #};
-#      grub = {
-#        enable = true;
-#        version = 2;
-#        device = "/dev/nvme0n1";
-#        useOSProber = true;
-#        extraEntries = '' 
-#        ''; 
-#      };
-#    };
+  ############
+  # Bootloader
+  ############
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
  
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      # assuming /boot is the mount point of the  EFI partition in NixOS (as the installation section recommends).
-      efiSysMountPoint = "/boot";
-    };
-    grub = {
-      # despite what the configuration.nix manpage seems to indicate,
-      # as of release 17.09, setting device to "nodev" will still call
-      # `grub-install` if efiSupport is true
-      # (the devices list is not used by the EFI grub install,
-      # but must be set to some value in order to pass an assert in grub.nix)
-      devices = [ "nodev" ];
-      efiSupport = true;
-      enable = true;
-      # set $FS_UUID to the UUID of the EFI partition
-      extraEntries = ''
-        menuentry "Windows" {
-          insmod part_gpt
-          insmod fat
-          insmod search_fs_uuid
-          insmod chain
-          search --fs-uuid --set=root $FS_UUID
-          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-        }
-      '';
-      version = 2;
-    };
-  };
-  
+  ######################
+  # Zen kernel (test me)
+  ######################
+  # boot.kernelPackages = pkgs.linuxPackages_zen;
+
+
+  #########
+  # Network 
+  #########
   networking.hostName = "blade"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+  # WPA supplicant 
+  #networking.wireless = { 
+  #  enable = true;
+  #  networks = {
+  #    Lumaca = {          
+  #      psk = "Fragile1.";
+  #    };
+  #  };
+  #};
+
+  ########
+  # Locale 
+  ########
 
   # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Rome";
   time.hardwareClockInLocalTime = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
-  networking.interfaces.wlo1.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.utf8";
+    LC_IDENTIFICATION = "it_IT.utf8";
+    LC_MEASUREMENT = "it_IT.utf8";
+    LC_MONETARY = "it_IT.utf8";
+    LC_NAME = "it_IT.utf8";
+    LC_NUMERIC = "it_IT.utf8";
+    LC_PAPER = "it_IT.utf8";
+    LC_TELEPHONE = "it_IT.utf8";
+    LC_TIME = "it_IT.utf8";
+  };
+
+  #######################
+  # Graphical Environment
+  #######################
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -89,82 +83,150 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  
-  # NVIDIA Settings
-  nixpkgs.config.allowUnfree = true;
-  hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.prime = {
-    offload.enable = true;
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
-
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-  };
+  #services.gnome = { 
+  #  core-utilities.enable = false;
+  #  tracker-miners.enable = false;
+  #  tracker.enable = false;
+  #};
 
   # Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
-  # Enable sound.
+  # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #jack.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  users.users.file0 = {
+    isNormalUser = true;
+    home = "/home/file0";
+    description = "Filippo";
+    extraGroups = [ "networkmanager" "wheel" "openrazer" "docker" ];
+    shell = pkgs.zsh;
+  };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.file0 = {
-     isNormalUser = true;
-     home = "/home/file0";
-     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-     description = "Filippo Di Gennaro";
-   };
+  home-manager.users.file0 = {
 
+    # USER PACKAGES
+    home.packages = with pkgs; [
+      discord
+      gotop
+      glxinfo
+      libreoffice
+      lollypop      
+      # Node  
+      nodejs
+      nodePackages.typescript
+      
+      spotify
+      steam
+      ranger
+      razergenie
+      vlc
+      tdesktop    
+      vscode
+      whatsapp-for-linux
+      #zsh
+      #zsh-syntax-highlighting
+      #zsh-completions
+      #zsh-autosuggestions
+      #oh-my-zsh
+    ];
+
+    programs.zsh = {
+      enable = true;
+      autocd = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+      enableAutosuggestions = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "gozilla";
+        plugins = [ "sudo" "git" "npm" "ng" "web-search" ];
+      };
+      shellAliases = {
+        "ll" = "ls -l";
+        ".." = "cd ..";
+        "update" = "sudo nixos-rebuild switch";
+	"edit" = "sudo nixos-rebuild edit";
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "file098";
+      userEmail = "filippodigennaro98@outlook.it";
+      aliases = {
+        st = "status";
+      };
+      ignores = [
+        ".DS_Store"
+        "*.pyc"
+      ];
+    };  
+  };
+
+  hardware.openrazer.enable = true;
+
+  # iOS
+  services.usbmuxd.enable = true;
+
+  # SYSTEM PACKAGES
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     # Core
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     firefox
-     git
-     neofetch
-     atom
-     # Gnome 
-     gnomeExtensions.dash-to-dock
-     gnomeExtensions.gsconnect
-     pkgs.gnome3.gnome-tweaks
-     # Other
-     tdesktop
-     discord
-     spotify
-     vlc
-     steam
-     alacritty
-     whatsapp-for-linux
-  ];
+  environment.systemPackages = with pkgs; [
+      # Core
+      alacritty
+      binutils
+      gcc
+      git
+      firefox
+      ifuse
+      libimobiledevice
+      ranger
+      neofetch
+      os-prober
+      openrazer-daemon
+      vim
+      wget
+      zip
+      unzip
+      # GNOME
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.tray-icons-reloaded
+      gnomeExtensions.sound-output-device-chooser
+      gnomeExtensions.pop-shell
+      pkgs.gnome3.gnome-tweaks
+      gparted
+      baobab
+   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+   fonts.fonts = with pkgs; [
+     fira-code
+     fira-code-symbols
+     noto-fonts
+     noto-fonts-emoji
+     source-code-pro
+   ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -172,27 +234,12 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh = {
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "thefuck" "sudo" "zsh-autosuggestions" "zsh-syntax-highlighting" "zsh-completions" "lol" ];
-      theme = "robbyrussell";
-    };
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
-  };  
-# This value determines the NixOS release from which the default
+  # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It‘s perfectly fine and recommended to leavecatenate(variables, "bootdev", bootdev)
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
-
-  system.autoUpgrade.enable = true;
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
-
